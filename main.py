2c1fb199b5c4ef04592c29cbd178cd893d7f5d2a
Giotto Bright <zenbukazeroka@gmail.com>
2023-11-19 01:52:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import google.generativeai as palm
 import os
 
 palm.configure(api_key='AIzaSyAH2RdrYVfsoXHYTycFkf7tCODLw9R3mZw')
-response = palm.generate_text(prompt='name 3 usa states')
-answer = response.result
-
 
 bot = telebot.TeleBot("6706341567:AAETyjnaiL0D7qfy7onS2EDeHCHIniaxPh4")
 
 @bot.message_handler(content_types=['text'])
 def echo(message):
-  bot.send_message(message.chat.id, answer)
+  prompt = message.text
+  response = palm.generate_text(prompt=prompt)
+  bot.send_message(message.chat.id, response.result)
 
 bot.polling()
